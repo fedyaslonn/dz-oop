910982b566b72c8a563122af1f02167c923efe46
fedyaslonn <133713839+fedyaslonn@users.noreply.github.com>
2024-04-08 00:03:19 +0300

    Add task-2 file

--- task-2/main.py
+++ task-2/main.py
@@ -1,1 +1,18 @@
-task-2
+class SuperStr(str):
+    def __init__(self, string):
+        self.string = string
+
+    def is_repeatanse(self, s):
+        if not s:
+            return False
+        repeat = len(self.string)// len(s)
+        return self.string == repeat * s
+
+    def is_palindrom(self):
+        return self.string.lower() == self.string[::-1].lower()
+
+
+check_string = SuperStr("abaaba")
+print(check_string.is_repeatanse("asjcsa"))
+print(check_string.is_repeatanse("aba"))
+print(check_string.is_palindrom())
