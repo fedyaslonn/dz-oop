94ee2ffd012f5d3ff9a2ebb3141c37ad7cd66192
fedyaslonn <133713839+fedyaslonn@users.noreply.github.com>
2024-04-08 00:19:44 +0300

    upload main.py

--- task-3/main.py
+++ task-3/main.py
@@ -1,37 +1,67 @@
-class Sphere:
-    def __init__(self, rad=None, x=None, y=None, z=None):
-        if rad is None and x is None and y is None and z is None:
-            self.rad = 1
-            self.x = 0
-            self.y = 0
-            self.z = 0
-            return
-        elif rad is not None and x is None and y is None and z is None:
-            self.rad = rad
-            self.x = 0
-            self.y = 0
-            self.z = 0
-        self.rad = rad
-        self.x = x
-        self.y = y
-        self.z = z
+class Bus:
+    def __init__(self, speed, max_seats, max_speed):
+        self.speed = speed
+        self.max_speed = max_speed
+        self.max_seats = max_seats
+        self.passengers = []
+        self.free_seats = True
+        self.seats = {}
+        self.free_seats_count = max_seats
+    def land_passengers(self, *passengers):
+        if len(self.passengers) + len(passengers) <= self.max_seats and self.free_seats_count >= len(passengers):
+            for passenger in passengers:
+                self.passengers.append(passenger)
+                self.seats[passenger] = len(self.passengers)
+                self.free_seats_count -= 1
+        else:
+            return f"Нету свободных мест"
 
-    def addition(self, value1, value2):
-        return value1 + value2
+    def kick_out_passengers(self, *passengers):
+        for passenger in passengers:
+            if passenger in self.passengers:
+                self.passengers.remove(passenger)
+                del self.seats[passenger]
+                self.free_seats_count += 1
+            else:
+                print(f"{passenger} нету в списке пассажиров")
 
-    def subtract(self, value1, value2):
-        return value1 - value2
 
-    def multi(self, value1, value2):
-        return value1 * value2
+    def increase_speed(self, value):
+        self.speed += value
 
-    def division(self, value1, value2):
-        if value2 == 0:
-            raise ZeroDivisionError("Деление на 0 недопустимо")
-        return value1 / value2
+    def decrease_speed(self, value):
+        self.speed -= value
 
-math = Math()
-print(math.multi(1,2))
-print(math.division(5,1))
-print(math.addition(3,10))
-print(math.subtract(5,4))
+    def __contains__(self, item):
+        return item in self.passengers
+
+    def __iadd__(self, passenger):
+        if isinstance(passenger, str):
+            self.land_passengers(passenger)
+            return self
+        else:
+            return f"Фамилия пассажира должна быть строковым типом"
+
+    def __isub__(self, passenger):
+        if isinstance(passenger, str):
+            self.kick_out_passengers(passenger)
+            return self
+        else:
+            return f"Фамилия пассажира должна быть строковым типом"
+
+    def __str__(self):
+        return f"Текущая скорость {self.speed}, список пассажиров {self.passengers}, словарь мест {self.seats}"
+
+bus = Bus(100, 30, 250)
+bus.land_passengers("Артем", "Настя", "Андрей", "Катя", "Никита", "Руслан")
+print(str(bus))
+bus += "Слава"
+print(str(bus))
+bus -= "Никита"
+print(str(bus))
+check_name = "Катя"
+print(check_name in bus)
+check_name2 = "Вика"
+print(check_name2 in bus)
+bus.increase_speed(34)
+print(str(bus))
